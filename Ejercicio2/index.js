1ece1fa9702c53168697e44568b5f57e9640514a
LinneteJuarez <mjuarezp@centro.edu.mx>
2024-10-01 19:27:14 -0600

    Sesion con todos los ejercios

---
+++ Ejercicio2/index.js
@@ -0,0 +1,32 @@
+console.log("hola")
+//1. Guardar una referencia a las etiquetas html en JavaScript
+
+var domMouseX = document.getElementById("coordX");
+var domMouseY = document.getElementById("coordY");
+
+var mouseX=0;
+var mouseY=0;
+
+console.log(domMouseX);
+console.log(domMouseY);
+
+//2.Definir nuestra funcion para imprimir coordenandas
+function printCoords(mouseData){
+  //recibir las coordenadas del mouse
+ console.log(mouseData.clientX);
+  console.log(mouseData.clientY);
+  //asignar al elemento HTML
+  mouseX=mouseData.clientX;
+  mouseY=mouseData.clientY;
+  
+  domMouseX.innerText = mouseData.clientX;
+  domMouseX.innerText = `Mouse X:${mouseData.clientX}`;
+  
+  domMouseY.innerText = mouseData.clientY;
+  domMouseY.innerText = `Mouse Y:${mouseData.clientY}`;
+   
+}
+
+//3.Escuchar el movimiento del mouse del usuario
+document.addEventListener("mousemove", printCoords);
+  
